506e3b9d9e9427a269df930e82a6a8010b00c4ac
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-03-04 14:45:42 +0100

    fetch

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
 console.log("test")
+
+async function getData () {
+    const response = await fetch('https://192.168.8.100/api/vxqLmDqnSVpUbzNSTo7WljD0VlnqzORDTyuxtuZY/lights/3/state');
+    const result = await response.json();
+    return result;
+};
+
+document.addEventListener('DOMContentLoaded', async() => {
+    const result = await getData();
+    console.log(result)
+});
