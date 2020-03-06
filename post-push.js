7292b3b096098919dc9ac2a481ce876e32c831ce
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-03-06 12:03:17 +0100

    test2

--- post-push.js
+++ post-push.js
@@ -1,12 +1,11 @@
-const v3 = require('node-hue-api').v3;
+const v3 = require("node-hue-api").v3;
 const LightState = v3.lightStates.LightState;
 
-const USERNAME = 'vxqLmDqnSVpUbzNSTo7WljD0VlnqzORDTyuxtuZY'
+const USERNAME = "vxqLmDqnSVpUbzNSTo7WljD0VlnqzORDTyuxtuZY",
   // The name of the light we wish to retrieve by name
-  , LIGHT_ID = 3
-;
-
-v3.discovery.nupnpSearch()
+  LIGHT_ID = 3;
+v3.discovery
+  .nupnpSearch()
   .then(searchResults => {
     const host = searchResults[0].ipaddress;
     return v3.api.createLocal(host).connect(USERNAME);
@@ -15,15 +14,12 @@ v3.discovery.nupnpSearch()
     // Using a LightState object to build the desired state
     const state = new LightState()
       .on(true)
-    //   .ct(200)
+      //   .ct(200)
       .brightness(50)
-      .hue(45000)
-      .sat(254)
-    ;
-    
+      .hue(65000)
+      .sat(254);
     return api.lights.setLightState(LIGHT_ID, state);
   })
   .then(result => {
     console.log(`Light state change was successful? ${result}`);
-  })
-;
+  });
