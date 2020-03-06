cb94b421473ed17b377a8e52cb88718b4fcc2d44
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-03-06 12:22:58 +0100

    tesst

--- pre-push.js
+++ pre-push.js
@@ -3,7 +3,8 @@ const LightState = v3.lightStates.LightState;
 
 const USERNAME = "vxqLmDqnSVpUbzNSTo7WljD0VlnqzORDTyuxtuZY",
   // The name of the light we wish to retrieve by name
-  LIGHT_ID = 3;
+  LIGHT_ID = 4;
+  
 v3.discovery
   .nupnpSearch()
   .then(searchResults => {
